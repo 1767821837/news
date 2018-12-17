2b3390a1367f1d98b4768507f642df1a935eb6ba
TYH <18380948178@163.com>
2018-12-17 21:23:06 +0800

    login

--- app/src/main/java/com/song/anew/activity/loginActivity.java
+++ app/src/main/java/com/song/anew/activity/loginActivity.java
@@ -8,6 +8,7 @@ import com.song.anew.R;
 public class loginActivity extends AppCompatActivity {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
