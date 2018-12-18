e2a83c8ec6766f88d0f61fcbe283d7e4537a5caa
17678 <1767821837@qq.com>
2018-12-18 12:07:07 +0800

    ddddd

--- app/src/main/java/com/song/anew/activity/RegisterActivity.java
+++ app/src/main/java/com/song/anew/activity/RegisterActivity.java
@@ -1,16 +1,37 @@
 package com.song.anew.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.song.anew.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    @BindView(R.id.et_tel)
+    EditText etTel;
+    @BindView(R.id.et_psd)
+    EditText etPsd;
+    @BindView(R.id.et_psd_er)
+    EditText etPsdEr;
+    @BindView(R.id.button)
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.button)
+    public void onViewClicked() {
 
     }
 }
